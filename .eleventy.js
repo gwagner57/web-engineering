1eebc4e66c09a4523a0facf298c85c63dc81a65a
leshinoe <118020640+leshinoe@users.noreply.github.com>
2023-07-11 15:12:46 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+
+  eleventyConfig.addWatchTarget("src/css/");
 
   return {
         dir: {
